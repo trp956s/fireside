dc2d0a8148af63cf5b5861c6211c5104598e24e7
Michael Dowden <michael@dowden.us>
2017-10-16 22:18:23 -0400

    Removed firebase config

--- src/client/app/services/firebase-ref.service.js
+++ src/client/app/services/firebase-ref.service.js
@@ -9,11 +9,6 @@
 
   // TODO - Initialize the Firebase App here [START-1]
   firebase.initializeApp({
-    apiKey: "AIzaSyAqvBkj8dpEL22TKMe8f2mfaL7MmEOcLcM",
-    authDomain: "app.playingwithfire.live",
-    databaseURL: "https://fireside-73c10.firebaseio.com/",
-    storageBucket: "fireside-73c10.appspot.com",
-    messagingSenderId: "811594146603"
   });
 
   /**
